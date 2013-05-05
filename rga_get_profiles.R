247a195ad2dcf473b0c8e6961d5f4663f8feac33
Manos Parzakonis <m.parzakonis@statsravingmad.com>
2013-05-05 19:52:06 +0300

    Create rga_get_profiles.R
    Intermediate step to get the profiles,web properties etc from the Management API

---
+++ rga_get_profiles.R
@@ -0,0 +1,7 @@
+# Use a call to the Management API to query your Profiles and select among them
+ga$getProfiles()
+
+# Define the ids and web properties to work with. The web.proporties is reserved for 
+# the name of the ids for labeling reasons etc
+ids<-c()
+web.properties<-c()
