e4155c9a759eb29e9a90997db013e6538d5e040a
Manos Parzakonis <m.parzakonis@statsravingmad.com>
2013-05-05 20:57:45 +0300

    Update rga_get_profiles.R

--- rga_get_profiles.R
+++ rga_get_profiles.R
@@ -1,7 +1,6 @@
 # Use a call to the Management API to query your Profiles and select among them
-ga$getProfiles()
+profiles<-ga$getProfiles()
 
 # Define the ids and web properties to work with. The web.proporties is reserved for 
 # the name of the ids for labeling reasons etc
-ids<-c()
-web.properties<-c()
+ids<-profiles<[1]
