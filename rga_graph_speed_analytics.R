58f6c13f9aeaf1704e791d9ffec1488d257dd24e
Manos Parzakonis <m.parzakonis@statsravingmad.com>
2013-05-05 21:09:08 +0300

    Update rga_graph_speed_analytics.R

--- rga_graph_speed_analytics.R
+++ rga_graph_speed_analytics.R
@@ -1,6 +1,6 @@
 # You realy got to open http://docs.ggplot2.org/ on a Browser tab now! It's awesome...
 # Need to fix xlab a bit
 m <- ggplot(data=subset(final_dataset,avgPLGroupNum<12), aes(x=avgPLGroup))
-m + geom_histogram(aes(y = ..count..)) + geom_density(fill=NA) + geom_histogram(aes(fill = ..count..)) + facet (. ~ pageGroup) 
+m + geom_histogram(aes(y = ..count..)) + geom_density(fill=NA) + geom_histogram(aes(fill = ..count..)) + facet_grid (. ~ pageGroup) 
 # Let's get a sense of the distribution of load times per month
-qplot(factor(yearmo), avgPageLoadTime, data = subset(final_dataset,yearmo>201208 & isMobile=='No'), geom = "boxplot",outlier.colour = "red", outlier.size = 1) + ylim(0, 20) + facet (. ~ pageGroup) + labs(title = "Page Load (in secs) for Desktop",x="Year-Month")
+qplot(factor(yearmo), avgPageLoadTime, data = subset(final_dataset,yearmo>201208 & isMobile=='No'), geom = "boxplot",outlier.colour = "red", outlier.size = 1) + ylim(0, 20) + facet_grid(. ~ pageGroup) + labs(title = "Page Load (in secs) for Desktop",x="Year-Month")
