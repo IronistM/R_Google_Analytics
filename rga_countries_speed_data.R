fbbb8fdf737dbd945fc2f67d89293a91b8e758f6
Manos Parzakonis <m.parzakonis@statsravingmad.com>
2013-05-05 23:18:41 +0300

    Update rga_countries_speed_data.R
    Not ready...

--- rga_countries_speed_data.R
+++ rga_countries_speed_data.R
@@ -1,5 +1,8 @@
 require(googleVis)
-input<- final_dataset
+
+# We need to aggreagate data across countries first
+# final_dataset_agg<-
+input<- final_dataset_aggr
 select<- input[which(input$avgPLGroupNum<18),]
 Map<- data.frame(select$country, select$avgPageLoadTime)
 names(Map)<- c("Country", "avgPageLoadTime")
